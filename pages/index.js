a17fddca7f4340a612f435d58d390432b1744540
Caridad Rivera <ccrive10@gmail.com>
2022-11-27 22:59:52 -0500

    css

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,11 @@ export default function Home() {
   <div className='flex flex-col w-screen h-screen m-8 p-8 bg-yellow shadow-xl '>      
      <h1 className='flex justify-center text-3xl m-8 font-extrabold'>Caridad Rivera</h1> 
           <div className="hexagon">
-            <div className='hexagon-background'>
               <Image 
                 src={myPic} 
                 alt=""
                   />
-            </div>
+          
           </div>
              <p className='text-fade animation ease 5s text-center p-8 m-8'>Software Engineer, critical thinker and fast-learner with a track record of translating design to code and building responsive, intuitive UIs.
               <hr className='my-8 h-px bg-gray-200 border-0 dark:bg-gray-700'/> I believe code is art and it has served as a tool of expression for me, which has allowed me to learn and grow while also having fun.</p>
