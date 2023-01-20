51a274533a3d9d5626947a5d05110a6269cda26b
Caridad Rivera <ccrive10@gmail.com>
2023-01-20 09:53:12 -0500

    styling

--- components/Main.js
+++ components/Main.js
@@ -7,17 +7,20 @@ import userData from "../constants/data"
 export default function Main() {
   const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
   return (
-    <div className="flex flex-row justify-center items-start overflow-hidden">
-      <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
-      <div className="flex flex-col">
-            <Link href="/">
-             <h1 className="font-semibold text-xl dark:text-gray-100">
-                {userData.name}
+  <div className="flex flex-row justify-center items-start overflow-hidden">
+      <div className="w-full md:w-1/2 sm:w-3/5 mt-40 pl-40 sm:-ml-30 ">
+        <div className="w-1/2 ">
+          <img src={userData.avatarUrl} alt="avatar" className=" shadow" />
+        </div>
+      </div>
+     <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+       <div className="flex flex-col">
+           <h1 className="font-semibold text-xl dark:text-gray-100">
+              {userData.name}
             </h1>
-             <p className="text-base font-light text-gray-500 dark:text-gray-300">
-                {userData.designation}
-             </p>
-            </Link>
+            <p className="text-base font-light text-gray-500 dark:text-gray-300">
+              {userData.designation}
+            </p>
         </div>
         <RoughNotationGroup show={true}>
           <RainbowHighlight color={colors[0]}>
@@ -36,11 +39,6 @@ export default function Main() {
             </h1>
           </RainbowHighlight>
         </RoughNotationGroup>
-      </div>
-      <div className="lg:block  w-full md:w-1/2 sm:w-1/4 -mr-30 mt-20 sm:-ml-30 ">
-        <div className=" w-3/5 ">
-          <img src={userData.avatarUrl} alt="avatar" className=" shadow" />
-        </div>
       </div>
     </div>
   );
